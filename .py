134698b53b15dc7df672a53692d13e148f659ffe
kondreddysaisampoorna <sampoorna1421@sasi.ac.in>
2024-08-01 15:36:37 +0530

    siri

---
+++ .py
@@ -0,0 +1,24 @@
+
+Numaric dataypes : 
+--------------------
+1. int
+2. float
+3. complex
+
+int : 
+	* Int represents an integer number. 
+	* It is number with out decimal part and fraction part
+	* There is no limit for size of int datatype. It can store any integer number conveniently.
+
+	Eg : 
+			>>> a = 20
+			>>> type(a)
+			<class 'int'>
+			>>> a = -234
+			>>> print(a)
+			-234
+			>>> a = 22222222222222222222222222222222222222222222222222222222
+			>>> print(a)
+			22222222222222222222222222222222222222222222222222222222
+			>>> type(a)
+			<class 'int'>
